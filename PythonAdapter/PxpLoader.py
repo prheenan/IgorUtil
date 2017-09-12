c7a687493acc01a13503f70a721e78230acdde36
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2017-09-12 11:38:00 -0600

    checking directory was not none

--- PythonAdapter/PxpLoader.py
+++ PythonAdapter/PxpLoader.py
@@ -229,6 +229,7 @@ def load_ibw_from_directory(in_dir,grouping_function,limit=None):
     Returns:
         dictionary: see GroupWavesByEnding, same output
     """
+    assert in_dir is not None , "input directory was None; can't load there."
     files = pGenUtil.getAllFiles(in_dir,ext=".ibw")
     ValidFunc= lambda *args,**keywords: True
     ibw_waves = [read_ibw_as_wave(f) for f in files]
